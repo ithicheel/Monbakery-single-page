9102e59e57d6e06d567dfe13316d6ea646015eb1
Nomin <40523815+nemuwn@users.noreply.github.com>
2021-12-01 21:02:08 +0800

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -69,9 +69,9 @@ function Footer() {
                             </p>
                         </li>
                         <li>
-                            <div>
-                                <input type="text" />
-                                <input type="submit" />
+                            <div class="inputbox">
+                                <input class="form_c" type="email" placeholder="И-мэйл" required="" name="EMAIL"/>
+                                <button class="btn" type="submit">ИЛГЭЭХ</button>
                             </div>
                         </li>
                     </ul>
@@ -126,7 +126,7 @@ function Footer() {
             </div>
             <div  className="footer_bottom">
                 <p>
-                    &copy 2021 Монбейкери бүх эрх хуулиар хамгаалагдсан
+                © 2021 Монбейкери бүх эрх хуулиар хамгаалагдсан
                 </p>
                 <div>
                     <i>
