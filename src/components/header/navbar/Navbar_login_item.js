90bc46ed64e52745e2892aee5a24e6daa8844f0f
Nomin <40523815+nemuwn@users.noreply.github.com>
2021-12-14 11:08:38 +0800

    Update Navbar_login_item.js

--- src/components/header/navbar/Navbar_login_item.js
+++ src/components/header/navbar/Navbar_login_item.js
@@ -2,9 +2,10 @@ import React, {useState} from 'react'
 import "./navbar_style.css"
 import { FaUser ,FaShoppingBag } from "react-icons/fa"
 import Modal from '../modal'
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 
-function Navbar_login_item(props) {
+function Navbar_login_item(props, onClose) {
     const [isOpen, setIsOpen] = useState(false)
 
     return (
@@ -15,6 +16,31 @@ function Navbar_login_item(props) {
         </div>
         <Modal open={isOpen} onClose={() => setIsOpen(false)}
         onRequestClose={() => setIsOpen(false)}>
+            <div className='formLogin' >
+          
+          <div class="session">
+            
+              <div class="left">  
+              </div>
+              <form action="" class="log-in" autocomplete="off">
+                
+                  <h4>We are <span>MONBAKERY</span></h4>
+                  <p>Тавтай Морил!</p>
+                  <div class="floating-label">
+                  <input placeholder="Email" type="text" name="email" id="email" autocomplete="off"/>
+                  <label for="email">Email:</label>
+                  </div>
+                  <div class="floating-label">
+                  <input placeholder="Password" type="password" name="password" id="password" autocomplete="off"/>
+                  <label for="password">Password:</label>
+                
+                  </div>
+                  <button className='loginBtn' type="submit" onClick="return false;">Log in</button>
+                  
+              </form>
+          </div>
+         
+        </div> 
        
         </Modal>
         
